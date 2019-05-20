f7ba260e729633d5342887c1bf9656a10b04e2d5
Maya Bose <47787632+boseHere@users.noreply.github.com>
2019-05-20 14:24:00 -0700

    Add files via upload

--- remove-error-reads.py
+++ remove-error-reads.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+'''
+Author: Maya Bose
+Date: 5/20/19
+This program removes the last base of error-prone full-length reads from a
+fastq file.
+'''
+
 
 import argparse
 import gzip
@@ -6,6 +13,7 @@ import gzip
 
 def main():
     d = get_args()
+    strip_error_bases(d)
 
 
 def get_args():
@@ -15,16 +23,27 @@ def get_args():
                                                  "genome")
     parser.add_argument('RNA file', metavar='n', type=str, nargs=1,
                         help='The RNA seq filename')
+    parser.add_argument('Target length', metavar='l', type=int, nargs=1, help=
+                        'Target length of a read')
     args = parser.parse_args()
     d = vars(args)
     return d
 
 
 def strip_error_bases(d):
-    with gzip.open(d['RNA file']) as rfile:
+    with gzip.open(d['RNA file'][0], 'r') as rfile:
+        i = 0
         for line in rfile:
             line = line.decode('utf-8')
             line = line.rstrip('\n')
-            if len(line) > 73:
+            if i == 1 or i == 3:
+                if len(line) > d['Target length'][0]:
+                    trim = len(line) - d['Target length'][0]
+                    line = line[:-1 * trim]
+            print(line)
+            i += 1
+            if i == 4:
+                i = 0
+
 
 main()
